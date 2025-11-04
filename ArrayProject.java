d187ac412b6b71f2254788114ac04a69e92ca4ef
zolotarnina58-ui <zolotarnina58@gmail.com>
2025-11-04 12:08:00 +0200

    Add files via upload

---
+++ ArrayProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrayproject;
+
+/**
+ *
+ * @author Student
+ */
+public class ArrayProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
